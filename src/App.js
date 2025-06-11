c7ed8d6472c868ccb6fe1c1f189c71ceebcdfc1e
Kbsit <kbsit@Kbsits-Mac-mini.bbrouter>
2025-06-11 01:15:24 -0700

    done

--- src/App.js
+++ src/App.js
@@ -4,19 +4,11 @@ import CountriesSearch from "./Components/CountriesSearch";
 
 function App() {
   const [countryData, setCountryData] = useState([]);
-  const [filterCountryData, setFilterCountryData] = useState([]);
-  const [searchText, setSearchText] = useState("");
-
-  const handleChange = (e) => {
-    setSearchText(e.target.value);
-  };
-
   const fetchCountryData = async () => {
     let url = "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries";
     try {
       let response = await axios.get(url);
       setCountryData(response.data);
-      setFilterCountryData(response.data);
     } catch (error) {
       console.log("Error: ", error);
     }
@@ -26,45 +18,16 @@ function App() {
     fetchCountryData();
   }, []);
 
-  const searchCountries = async () => {
-    if (searchText === "") {
-      setFilterCountryData(countryData);
-    }
 
-    let url = "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries";
 
-    try {
-      let response = await axios.get(url);
 
-      const filteredData = response.data.filter((country) =>
-        country.common.toLowerCase().includes(searchText.toLowerCase())
-      );
-
-      setFilterCountryData(filteredData);
-    } catch (error) {
-      console.log("Error: ", error);
-    }
-  };
-
-  useEffect(() => {
-    searchCountries();
-  }, [searchText]);
 
   return (
     <div>
-      <div className="searchSection">
-        <form>
-          <input
-            type="text"
-            placeholder="Search for countries..."
-            value={searchText}
-            onChange={(e) => handleChange(e)}
-          />
-        </form>
-      </div>
+     
       <div className="App">
-        {filterCountryData &&
-          filterCountryData.map((ele) => <CountriesSearch data={ele} />)}
+        {countryData &&
+          countryData.map((ele) => <CountriesSearch data={ele} />)}
       </div>
     </div>
   );
