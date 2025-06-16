00b05108265d6497aa2fa13c11c082c428717667
Kbsit <kbsit@Kbsits-Mac-mini.local>
2025-06-16 03:24:37 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
-import './App.css';
-import CounterApp from "./Counter.jsx";
+import { Outlet } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import Footer from "./components/Footer/Footer";
+import DownloadApp from "./components/Sections/DownloadApp/DownloadApp";
 
 function App() {
   return (
-    <CounterApp/>
+    <div>
+      <CssBaseline />
+      <Outlet />
+      <DownloadApp />
+      <Footer />
+    </div>
   );
 }
 
