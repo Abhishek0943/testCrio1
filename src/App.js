0a19f73893abf65baf46ef6058f3984758d83d42
Abhishek <Abhishek>
2025-06-22 22:16:33 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import './App.css';
-import Layout from "./Components/Layout/Layout";
-import Home from "./Pages/Home/Home";
-import Search from "./Pages/Search/Search";
-import Booking from "./Pages/Booking/Booking";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { SnackbarProvider } from 'notistack'
-
-// write theme
-const theme = createTheme({
-  palette: {
-    primary: {
-      main : "#2AA7FF",
-      contrastText: "#ffffff"
-    },
-  }
-})
+import { Outlet } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import Footer from "./components/Footer/Footer";
+import DownloadApp from "./components/Sections/DownloadApp/DownloadApp";
 
 function App() {
   return (
-    <SnackbarProvider
-    maxSnack={1}
-    >
-    
-    <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />} >
-          <Route index element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/my-bookings" element={<Booking />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </ThemeProvider>
-    
-    </SnackbarProvider>
+    <div>
+      <CssBaseline />
+      <Outlet />
+      <DownloadApp />
+      <Footer />
+    </div>
   );
 }
 
