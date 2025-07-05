a450efcbf9ddb9af1e44fcd0e57c6988d83b43bc
Kbsit <kbsit@Kbsits-Mac-mini.bbrouter>
2025-07-05 03:10:21 -0700

    done

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import ConversationHistory from './pages/history';
+import ChatWindow from './pages/home';
+import { ThemeProvider, createTheme } from '@mui/material';
+import Modal from "react-modal"
+
+// set modal root
+Modal.setAppElement("#root");
+
+
+
+const router = createBrowserRouter([{
+
+  path: "/",
+  element: <App />,
+  children: [
+    {
+      path: "history",
+      element: <ConversationHistory />
+    },
+    {
+      path: "/",
+      element: <ChatWindow />
+    }
+  ]
+}])
+
+const theme = createTheme({
+
+  typography: {
+    fontFamily: `"Ubuntu", "Open Sans", sans-serif`
+  },
+  palette: {
+    primary: {
+      main: "#9785BA",
+      light: "#AF9FCD",
+      contrastText: "black"
+    },
+    secondary: {
+      main: "#D7C7F4",
+      contrastText: "black"
+    }
+  }
+
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    
+    <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+    </ThemeProvider>
+    
+  </React.StrictMode>
+);
+
+
---
+++ src/pages/home.js
@@ -0,0 +1,90 @@
+import { Box, Typography, IconButton, Stack } from "@mui/material"
+import MenuIcon from "@mui/icons-material/Menu";
+import { useOutletContext } from "react-router-dom";
+import ChatCard from "../components/ChatCard/ChatCard";
+import Welcome from "../components/Welcome/Welcome";
+
+const dummyData = {
+    type: "chat",
+    isUser: false,
+    time: "10:33 am",
+    content: "Lorem ipsum"
+}
+
+// AI Chat
+export default function ChatWindow() {
+
+    const { isMobile, setMenuOpen, currentConvo, bottomRef } = useOutletContext();
+    
+    
+
+    return (
+        <Box
+        sx={{
+            flexGrow: 1,
+            display: "flex",
+            flexDirection: "column"
+        }}
+        >
+            <Stack direction="row" spacing={2}
+            sx={{
+                padding: "5px",
+                alignItems: "center",
+                marginLeft: "20px"
+            }}
+            >
+
+                {
+                    isMobile && (
+                        <IconButton
+                        onClick={() => {
+                            setMenuOpen(true);
+                        }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                    )
+                }
+                
+                <header>
+                    <Typography
+                    variant="h1"
+                    fontSize="3rem"
+                    fontWeight="700"
+                    color="primary"
+                    >Bot AI</Typography>
+                </header>
+
+                
+
+            </Stack>
+
+            <Box
+            sx={{
+                flexGrow: 1,
+                overflowY: "auto",
+                display: "flex",
+                flexDirection: "column",
+                
+            }}
+            >
+
+                {/* if convo empty, then welcome */}
+                {
+                    (currentConvo.length > 0) || (
+                        <Welcome />
+                    )
+                }
+
+
+                {/* Convo chats */}
+                {
+                    currentConvo.map((convo, index) => <ChatCard key={index} type="chat" isUser={convo.isUser} time={convo.time} content={convo.content} />)
+                }
+
+                <div ref={bottomRef} ></div>
+            </Box>
+            
+        </Box>
+    )
+}
