c7af9e5b4eca7d49e9f44127ec68d9f7efa616ca
Kbsit <kbsit@Kbsits-Mac-mini.bbrouter>
2025-07-04 22:31:38 -0700

    done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,83 +1,145 @@
-import {
-    Box,
-    Button,
-    Container,
-    Grid,
-    Link,
-    Stack,
-    Typography,
-  } from "@mui/material";
-  import logo from "../../assets/logo.png";
-  import fb from "../../assets/fb.png";
-  import pinterest from "../../assets/pinterest.png";
-  import twitter from "../../assets/twitter.png";
-  import yt from "../../assets/yt.png";
-  import FooterLink from "./FooterLink";
-  
-  export default function Footer() {
-    return (
-      <Box bgcolor="primary.secondary" pb={3} pt={6}>
-        <Container maxWidth="xl">
-          <Grid container spacing={4}>
-            <Grid item xs={12} md={4.5}>
-              <Stack
-                alignItems="flex-start"
-                justifyContent="space-between"
-                height={1}
-              >
-                <Box src={logo} height={36} alt="Medify" component="img" mb={2} />
-                <Stack direction="row" spacing={1.5}>
-                  <Box component="img" src={fb} height={36} />
-                  <Box component="img" src={twitter} height={36} />
-                  <Box component="img" src={yt} height={36} />
-                  <Box component="img" src={pinterest} height={36} />
-                </Stack>
-              </Stack>
-            </Grid>
-  
-            <Grid item xs={12} md={2.5}>
-              <Stack spacing={2}>
-                <FooterLink>About Us</FooterLink>
-                <FooterLink>Our Pricing</FooterLink>
-                <FooterLink>Our Gallery</FooterLink>
-                <FooterLink>Appointment</FooterLink>
-                <FooterLink>Privacy Policy</FooterLink>
-              </Stack>
-            </Grid>
-  
-            <Grid item xs={12} md={2.5}>
-              <Stack spacing={2}>
-                <FooterLink>Orthology</FooterLink>
-                <FooterLink>Neurology</FooterLink>
-                <FooterLink>Dental Care</FooterLink>
-                <FooterLink>Opthalmology</FooterLink>
-                <FooterLink>Cardiology</FooterLink>
-              </Stack>
-            </Grid>
-  
-            <Grid item xs={12} md={2.5}>
-              <Stack spacing={2}>
-                <FooterLink>About Us</FooterLink>
-                <FooterLink>Our Pricing</FooterLink>
-                <FooterLink>Our Gallery</FooterLink>
-                <FooterLink>Appointment</FooterLink>
-                <FooterLink>Privacy Policy</FooterLink>
-              </Stack>
-            </Grid>
-          </Grid>
-  
-          <Typography
-            fontWeight={300}
-            color="#fff"
-            fontSize={14}
-            pt={3}
-            mt={5}
-            borderTop="1px solid rgba(255,255,255,0.1)"
-          >
-            Copyright ©2023 Surya Nursing Home.com. All Rights Reserved
-          </Typography>
-        </Container>
-      </Box>
-    );
-  }
-  
+import styles from "./Footer.module.css";
+import dentist from "../../assets/dentist.svg";
+import bloodtest from "../../assets/bloodtest.svg";
+import cardiology from "../../assets/cardiology.svg";
+import labrotary from "../../assets/labrotary.svg";
+import mri from "../../assets/mri.svg";
+import piscologist from "../../assets/piscologist.svg";
+import xray from "../../assets/x-ray.svg";
+import primaryCare from "../../assets/primaryCare.svg";
+import patientimage from "../../assets/patientCaring.svg";
+import tick from "../../assets/tick.svg";
+import group_1 from "../../assets/group_1.svg";
+
+import Card from "../Card/Card";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.specialization}>
+        <div>
+          <span className={styles.heading}>Find By Specialisation</span>
+        </div>
+        <div className={styles.specialGrid}>
+          <div>
+            <img src={dentist} alt="" />
+          </div>
+          <div>
+            <img src={primaryCare} alt="" />
+          </div>
+          <div>
+            <img src={cardiology} alt="" />
+          </div>
+          <div>
+            <img src={mri} alt="" />
+          </div>
+          <div>
+            <img src={bloodtest} alt="" />
+          </div>
+          <div>
+            <img src={piscologist} alt="" />
+          </div>
+          <div>
+            <img src={labrotary} alt="" />
+          </div>
+          <div>
+            <img src={xray} alt="" />
+          </div>
+        </div>
+        <button>View All</button>
+      </div>
+      <div className={styles.medicalSpecialist}>
+        <div>
+          <span className={styles.heading}>Our Medical Specialist</span>
+        </div>
+        <div className={styles.swiper}></div>
+        <div></div>
+      </div>
+
+      {/* --------------- */}
+
+      <div className={styles.patientcaring}>
+        <div className={styles.patientImg}>
+          <img src={patientimage} alt="image" />
+        </div>
+        <div className={styles.pateintText}>
+          <div className={styles.heading_1}>
+            HELPING PATIENTS FROM AROUND THE GLOBE!!
+          </div>
+          <div className={styles.heading_2}>
+            <span>Patient</span>
+            <span> Caring</span>
+          </div>
+          <div className={styles.patientPara}>
+            Our goal is to deliver quality of care in a courteous, respectful,
+            and compassionate manner. We hope you will allow us to care for you
+            and strive to be the first and best choice for healthcare.
+          </div>
+          <div className={styles.highlights}>
+            <div>
+              <span className={styles.tickImage}>
+                <img src={tick} alt="image" />
+              </span>
+              <span className={styles.highlightsText}>
+                Stay Updated About Your Health
+              </span>
+            </div>
+            <div>
+              <span className={styles.tickImage}>
+                <img src={tick} alt="image" />
+              </span>
+              <span className={styles.highlightsText}>
+                Check Your Results Online
+              </span>
+            </div>
+            <div>
+              <span className={styles.tickImage}>
+                <img src={tick} alt="image" />
+              </span>
+              <span className={styles.highlightsText}>
+                Manage Your Appointments
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* -------------- */}
+      <div className={styles.newsSection}>
+        <div className={styles.newsHeading}>
+          <div className={styles.newsSubHeading}>Blog & News</div>
+          <div className={styles.newsMainHeading}>Read Our Latest News</div>
+        </div>
+        <div className={styles.cardSection}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+      {/* ---families section */}
+      <div className={styles.families}>
+        <div className={styles.familiesText}>
+          <div className={styles.familiesSubHeading}>
+            CARING FOR THE HEALTH OF YOU AND YOUR FAMILY.
+          </div>
+          <div className={styles.familiesMainHeading}>Our Families</div>
+          <div className={styles.familiesPara}>
+            We will work with you to develop individualised care plans,
+            including management of chronic diseases. If we cannot assist, we
+            can provide referrals or advice about the type of practitioner you
+            require. We treat all enquiries sensitively and in the strictest
+            confidence.
+          </div>
+        </div>
+        <div className={styles.familiesImg}>
+          <img src={group_1} alt="image" />
+        </div>
+      </div>
+      {/* ------------------ */}
+      <div className="faqs"></div>
+      <div className="medifyApp"></div>
+      <div className="mainFooter"></div>
+    </div>
+  );
+};
+
+export default Footer;
