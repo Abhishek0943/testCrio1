518eb6bdd47dfcd753d600463656f368e224f5fd
abhishek0943 <abhishekpundir077@gmail.com>
2025-07-21 12:43:24 +0530

    inital commit xwather

--- src/App.js
+++ src/App.js
@@ -1,45 +1,76 @@
 import { useEffect, useState } from "react";
-// import "./styles.css";
+import "./styles.css";
+const key = "624a02b1b8d54df3a44152348232710";
 
+function Card({ name, value }) {
+  return (
+    <div className="weather-card">
+      <b>{name}</b>
+      <p>{value}</p>
+    </div>
+  );
+}
 export default function App() {
-  const [isRunning, setRunning] = useState(false);
-  const [currentTime, setCurrentTime] = useState(0);
-
+  const [city, setCity] = useState("");
+  const [isCityAdded, setCityAdded] = useState(false);
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    let interval = null;
-    if (isRunning) {
-      interval = setInterval(() => {
-        setCurrentTime((prevTime) => prevTime + 1);
-        // console.log(currentTime);
-      }, 1000);
-    } else if (!isRunning && currentTime !== 0) {
-      clearInterval(interval);
+    if (isCityAdded) {
+      try {
+        async function fetchApi() {
+          const res = await fetch(
+            `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`
+          )
+            .then((res) => res.json())
+            .then((data) => {
+              setData(data);
+              return data;
+            });
+          if (res.error) {
+            alert("Failed to fetch weather data");
+            setCityAdded(false);
+          }
+          setLoading(false);
+        }
+        fetchApi();
+      } catch (err) {
+        console.log(err);
+        alert("Failed to fetch weather data");
+        setCityAdded(false);
+      } finally {
+        setCityAdded(false);
+      }
     }
-    return () => clearInterval(interval);
-  }, [isRunning]);
+  }, [isCityAdded, city]);
 
-  const resetTimer = () => {
-    setRunning(false);
-    setCurrentTime(0);
-  };
-
-  const toggle = () => {
-    setRunning((prevState) => !prevState);
-  };
-
-  const formatTime = (time) => {
-    // console.log(time);
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    return `${minutes}:${seconds.toString().padStart(2, "0")}`;
-  };
-
+  function handleButton(e) {
+    e.preventDefault();
+    console.log("Clicked");
+    console.log(city);
+    setLoading(true);
+    setCityAdded(true);
+  }
   return (
     <div className="App">
-      <h1>Stopwatch</h1>
-      <p className="time">Time: {formatTime(currentTime)}</p>
-      <button onClick={toggle}>{isRunning ? "Stop" : "Start"}</button>
-      <button onClick={resetTimer}>Reset</button>
+      <form action="" onSubmit={handleButton}>
+        <input
+          name="search"
+          type="text"
+          placeholder="Enter city name"
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {loading && <p>Loading data...</p>}
+      {data.current && !loading && (
+        <div className="weather-cards">
+          <Card name={"Temperature"} value={data?.current?.temp_c + "°C"} />
+          <Card name={"Humidity"} value={data?.current?.humidity + "%"} />
+          <Card name={"Condition"} value={data?.current?.condition.text} />
+          <Card name={"Wind Speed"} value={data?.current?.wind_kph + " kph"} />
+        </div>
+      )}
     </div>
   );
 }
