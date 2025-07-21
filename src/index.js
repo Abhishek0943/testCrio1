6a4d4cbf00047c272e8affb94ec9bdc233438fec
abhishek0943 <abhishekpundir077@gmail.com>
2025-07-21 12:15:35 +0530

    inital commint xlogin

--- src/index.js
+++ src/index.js
@@ -1,62 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import ConversationHistory from './pages/history';
-import ChatWindow from './pages/home';
-import { ThemeProvider, createTheme } from '@mui/material';
-import Modal from "react-modal"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-// set modal root
-Modal.setAppElement("#root");
+import App from "./App";
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-
-const router = createBrowserRouter([{
-
-  path: "/",
-  element: <App />,
-  children: [
-    {
-      path: "history",
-      element: <ConversationHistory />
-    },
-    {
-      path: "/",
-      element: <ChatWindow />
-    }
-  ]
-}])
-
-const theme = createTheme({
-
-  typography: {
-    fontFamily: `"Ubuntu", "Open Sans", sans-serif`
-  },
-  palette: {
-    primary: {
-      main: "#9785BA",
-      light: "#AF9FCD",
-      contrastText: "black"
-    },
-    secondary: {
-      main: "#D7C7F4",
-      contrastText: "black"
-    }
-  }
-
-})
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    
-    <ThemeProvider theme={theme}>
-        <RouterProvider router={router} />
-    </ThemeProvider>
-    
-  </React.StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
-
-
